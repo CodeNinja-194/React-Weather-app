05c280a8458998925c571cc59e1a46b47e0d4a15
Sri Ram <your.email@example.com>
2023-09-21 07:09:55 +0530

    first commit

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://prasadcodetech-194.vercel.app/"
+        > Sri Ram Prasad Adusumilli</a
+      >, <a href="https://github.com/CodeNinja-194/React-Weather-App">Open sourced on Github.</a> Hosted on Netlify
+    </p>
+  </div>
+  );
+}
